23375af68ff8154a83ef52669746bdbf0ee17f43
Alexandre Higino <alehigino@yahoo.com.br>
2021-03-29 22:52:24 -0300

    Configuracao das demais classes

--- app/src/main/java/br/com/progiv/trabalhofinal/RepositorioPessoaScript.java
+++ app/src/main/java/br/com/progiv/trabalhofinal/RepositorioPessoaScript.java
@@ -1,4 +1,49 @@
 package br.com.progiv.trabalhofinal;
 
-public class RepositorioPessoaScript {
+import android.content.Context;
+
+public class RepositorioPessoaScript extends RepositorioPessoa {
+
+    // Script para fazer drop na tabela
+    private static final String SCRIPT_DATABASE_DELETE = "DROP TABLE IF EXISTS pessoa";
+
+    // Cria a tabela com o "_id" sequencial
+    private static final String[] SCRIPT_DATABASE_CREATE = new String[] {
+        "create table pessoa (_id integer primary key autoincrement, nome text not null, cpf text not null, idade text not null);",
+        "insert into pessoa(nome, cpf, idade) values('Alexandre Higino','01234567890', 27);",
+        "insert into pessoa(nome, cpf, idade) values('Amanda Oliveira','09876543210', 24);",
+        "insert into pessoa(nome, cpf, idade) values('Eduardo','55533322211', 55);" };
+
+    // Nome do banco
+    private static final String NOME_BANCO = "base_pessoas";
+
+    // Controle de versão
+    private static final int VERSAO_BANCO = 2;
+
+    // Nome da tabela
+    public static final String TABELA_PESSOA = "pessoa";
+
+    // Classe utilitária para abrir, criar, e atualizar o banco de dados
+    private SQLiteHelper dbHelper;
+
+    // Cria o banco de dados com um script SQL
+    public RepositorioPessoaScript(Context ctx) {
+        // Criar utilizando um script SQL
+        dbHelper = new SQLiteHelper(ctx, RepositorioPessoaScript.NOME_BANCO,
+        RepositorioPessoaScript.VERSAO_BANCO,
+        RepositorioPessoaScript.SCRIPT_DATABASE_CREATE,
+        RepositorioPessoaScript.SCRIPT_DATABASE_DELETE);
+
+        // abre o banco no modo escrita para poder alterar também
+        db = dbHelper.getWritableDatabase();
+    }
+
+    // Fecha o banco
+    @Override
+    public void fechar() {
+        super.fechar();
+        if (dbHelper != null) {
+            dbHelper.close();
+        }
+    }
 }
--- app/src/main/java/br/com/progiv/trabalhofinal/SQLiteHelper.java
+++ app/src/main/java/br/com/progiv/trabalhofinal/SQLiteHelper.java
@@ -1,4 +1,48 @@
 package br.com.progiv.trabalhofinal;
 
-public class SQLiteHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+class SQLiteHelper extends SQLiteOpenHelper {
+
+    private String[] scriptSQLCreate;
+    private String scriptSQLDelete;
+
+    /**
+     * Cria uma instância de SQLiteHelper
+     *
+     * @param context
+     * @param nomeBanco nome do banco de dados
+     * @param versaoBanco versão do banco de dados (se for diferente é para atualizar)
+     * @param scriptSQLCreate SQL com o create table..
+     * @param scriptSQLDelete SQL com o drop table...
+     */
+    SQLiteHelper(Context context, String nomeBanco, int versaoBanco, String[]
+            scriptSQLCreate, String scriptSQLDelete) {
+        super(context, nomeBanco, null, versaoBanco);
+        this.scriptSQLCreate = scriptSQLCreate;
+        this.scriptSQLDelete = scriptSQLDelete;
+    }
+
+    @Override
+    // Criar novo banco...
+    public void onCreate(SQLiteDatabase db) {
+        // Executa cada sql passado como parâmetro
+        for (int i = 0; i < scriptSQLCreate.length; i++) {
+            String sql = scriptSQLCreate[i];
+
+            // Cria o banco de dados executando o script de criação
+            db.execSQL(sql);
+        }
+    }
+
+    @Override
+    // Mudou a versão...
+    public void onUpgrade(SQLiteDatabase db, int versaoAntiga, int novaVersao) {
+        // Deleta as tabelas...
+        db.execSQL(scriptSQLDelete);
+        // Cria novamente...
+        onCreate(db);
+    }
 }
